e588b107ead40c73a0640fadac6d157e141485a7
Ahmad Riyad <ahmadriyad46@gmail.com>
2023-01-26 02:26:34 +0700

    FIXX

--- dashboard/bantuan.php
+++ dashboard/bantuan.php
@@ -13,13 +13,13 @@ include('./template/header.php');
                     <td> - Menu Dashboard merupakan stastik dan penggunaan aplikasi ini</td>
                 </tr>
                 <tr>
-                    <td> - Menu File terbagi 2 yakni Enkripsi dan Dekripsi</td>
+                    <td> - Menu Berkas terbagi 2 yakni Enkripsi dan Dekripsi</td>
                 </tr>
                 <tr>
-                    <td> - Untuk Mengenkripsi file pilih pada menu File -> Enkripsi</td>
+                    <td> - Untuk Mengenkripsi file pilih pada menu Berkas -> Enkripsi</td>
                 </tr>
                 <tr>
-                    <td> - Untuk Mengdekripsi file pilih pada menu File -> Dekripsi</td>
+                    <td> - Untuk Mengdekripsi file pilih pada menu Berkas -> Dekripsi</td>
                 </tr>
                 <tr>
                     <td> - Menu Daftar Hasil merupkan menu untuk melihat daftar list file yang telah dienkripsi dan
