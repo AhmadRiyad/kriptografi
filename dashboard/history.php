32897ddc9db28551693a74ab90425eceeae48869
AhmadRiyad <ahmadriyad46@gmail.com>
2023-01-25 21:53:28 +0700

    FIXX

--- dashboard/history.php
+++ dashboard/history.php
@@ -1,4 +1,4 @@
-<?php include('../config.php');?>
+<?php include('../config.php'); ?>
 <?php include 'template/header.php'; ?>
 
 <div class="card mt-3">
@@ -15,6 +15,8 @@
                         <th style="vertical-align:middle;">Nama File Asli</th>
                         <th style="vertical-align:middle;">File Enkripsi</th>
                         <th style="vertical-align:middle;">Ukuran File</th>
+                        <th style="vertical-align:middle;">Waktu Enkripsi</th>
+                        <th style="vertical-align:middle;">Waktu Dekripsi</th>
                         <th style="vertical-align:middle;">Keterangan</th>
                         <th style="vertical-align:middle;">Status</th>
                         <th style="vertical-align:middle;">Delete</th>
@@ -22,9 +24,9 @@
                 </thead>
                 <tbody>
                     <?php
-              $no =1;
-              $query = mysqli_query($connect,"SELECT * FROM file");
-              while ($data = mysqli_fetch_array($query)) { ?>
+                    $no = 1;
+                    $query = mysqli_query($connect, "SELECT * FROM file");
+                    while ($data = mysqli_fetch_array($query)) { ?>
                     <tr>
                         <td class="text-center" style="vertical-align:middle;"><?php echo $no++; ?></td>
                         <td style="vertical-align:middle;"><?php echo $data['username']; ?></td>
@@ -32,16 +34,18 @@
                         <?php $namabrks = $data['file_name_source']; ?>
                         <td style="vertical-align:middle;"><?php echo $data['file_name_finish']; ?></td>
                         <td style="vertical-align:middle;"><?php echo $data['file_size']; ?> KB</td>
+                        <td style="vertical-align:middle;"><?php echo $data['durasi_proses_enkripsi']; ?> Detik</td>
+                        <td style="vertical-align:middle;"><?php echo $data['durasi_proses_dekripsi']; ?> Detik</td>
                         <td style="vertical-align:middle;"><?php echo $data['keterangan']; ?></td>
                         <td style="vertical-align:middle;"><?php if ($data['status'] == 1) {
-                    echo "<a href='javascript:void(0)' class='btn btn-success btn-sm'><i class='fa fa-check'></i> Terenkripsi</a>";
-                  }elseif ($data['status'] == 2) {
-                    echo "<a href='file_decrypt/$namabrks' class='btn btn-warning btn-sm' download><i class='fa fa-download'></i> Download</a>     
+                                                                    echo "<a href='javascript:void(0)' class='btn btn-success btn-sm'><i class='fa fa-check'></i> Terenkripsi</a>";
+                                                                } elseif ($data['status'] == 2) {
+                                                                    echo "<a href='file_decrypt/$namabrks' class='btn btn-warning btn-sm' download><i class='fa fa-download'></i> Download</a>     
                     ";
-                  }else {
-                    echo "<span class='btn btn-danger'>Status Tidak Diketahui</span>";
-                  }
-                  ?></td>
+                                                                } else {
+                                                                    echo "<span class='btn btn-danger'>Status Tidak Diketahui</span>";
+                                                                }
+                                                                ?></td>
                         <td>
                             <a onClick="return confirm('Data ini akan di hapus.?')"
                                 href="delete.php.?id=<?php echo $data['id_file']; ?>" class="btn btn-danger btn-sm">
@@ -50,10 +54,10 @@
                         </td>
                     </tr>
                     <?php
-              } ?>
+                    } ?>
                 </tbody>
             </table>
         </div>
     </div>
 </div>
-<?php include 'template/footer.php'?>
+<?php include 'template/footer.php' ?>
