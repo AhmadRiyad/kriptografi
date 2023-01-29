47d1bb69f2c7d825e6a7031bad480e20da1402f0
Fajar Firdaus <daussfajar28@gmail.com>
2023-01-30 02:17:09 +0700

    Benerin logout

--- dashboard/logout.php
+++ dashboard/logout.php
@@ -5,11 +5,10 @@
 	$cekUser = $connect->query("SELECT username FROM users WHERE username = '$username'")->fetch_assoc();
 	$now = date('Y-m-d H:i:s');
 	if(!empty($cekUser)){
-		$update = $connect->query("UPDATE users SET last_activity = '$now' WHERE username = '$username'");
-		if($update === true){
-			session_unset();
-			session_destroy();
-			header('location: ../index.php');
-		}
+		$update = $connect->query("UPDATE users SET last_activity = '$now' WHERE username = '$username'");		
 	}	
+
+	session_unset();
+	session_destroy();
+	header('location: ../index.php');
  ?>
